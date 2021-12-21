07682aaa9b3e53e097debccd58c65d043c9ebebb
1305 <64506392+13-05@users.noreply.github.com>
2021-12-21 00:10:56 -0500

    lmao the last fix didnt work
    this commit SHOULD fix webhook validity checking
    
    if it doesnt, idk what to do lol

--- selfbot_red.py
+++ selfbot_red.py
@@ -79,9 +79,9 @@ def red_start():
         "username" : "Red Calibration Sequence"
     }
   y = requests.post(b, json=hook_data)
-  if int(y.status_code) == 200:
+  if int(y.status_code) == 204:
     print(f"{Fore.BLUE}Hook is valid, continuing Red's startup.")
-  else:
+  elif int(y.status_code) == 404:
     print(f"{Fore.YELLOW}The webhook URL located in red_hook.txt is invalid. Replace it with a valid discord webhook, then Red can run.")
     exit(0)
 
